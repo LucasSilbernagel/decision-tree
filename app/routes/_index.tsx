7bb56610b09ce2874f3318541f28120025208ef2
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-10-16 13:29:54 -0400

    added decision tree title edit functionality

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from '@remix-run/node'
-import { useState } from 'react'
+import { Pencil, Save } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import { Button } from '~/components/ui/button'
 import { Card } from '~/components/ui/card'
 import { Input } from '~/components/ui/input'
@@ -25,6 +26,10 @@ export default function Index() {
     node: DecisionTreeNode
   } | null>(null)
 
+  const [decisionTreeTitleDraft, setDecisionTreeTitleDraft] = useState(
+    decisionTree?.title.value || ''
+  )
+
   const getHighestId = (node: DecisionTreeNode | null): number => {
     if (node === null) {
       return -1 // Return a default value for null branches
@@ -36,7 +41,7 @@ export default function Index() {
 
   const startNewDecisionTree = () => {
     setDecisionTree({
-      title: { value: 'Decision Tree', isEditing: false },
+      title: { value: 'Decision Tree Title', isEditing: false },
       node: {
         id: 0,
         text: { value: 'Yes or no?', isEditing: false },
@@ -55,6 +60,18 @@ export default function Index() {
     }
   }
 
+  const handleTitleEdit = () => {
+    if (decisionTree) {
+      setDecisionTree({
+        ...decisionTree,
+        title: {
+          value: decisionTreeTitleDraft || 'Decision Tree Title',
+          isEditing: !decisionTree.title.isEditing,
+        },
+      })
+    }
+  }
+
   const renderNode = (
     node: DecisionTreeNode,
     updateNode: (newNode: DecisionTreeNode) => void
@@ -62,7 +79,7 @@ export default function Index() {
     return (
       <div className="relative flex flex-col items-center mb-12">
         {/* Main node */}
-        <Card className="flex flex-col items-center border border-gray-300 rounded-lg bg-gray-50 shadow-sm w-[300px]">
+        <Card className="flex flex-col items-center border-gray-300 bg-gray-50 shadow-sm border rounded-lg w-[300px]">
           <div>
             <Label htmlFor={`condition-${node.id}`} className="sr-only">
               Condition {node.id}
@@ -82,11 +99,11 @@ export default function Index() {
         </Card>
 
         {/* Yes and No branches */}
-        <div className="relative flex justify-center w-full mt-8">
+        <div className="relative flex justify-center mt-8 w-full">
           {/* No branch (Left) */}
-          <div className="flex flex-col items-center w-[300px] mx-8">
-            <strong className="text-red-600 mb-2">No:</strong>
-            <div className="w-[300px] flex-grow">
+          <div className="flex flex-col items-center mx-8 w-[300px]">
+            <strong className="mb-2 text-red-600">No:</strong>
+            <div className="flex-grow w-[300px]">
               {node.no ? (
                 renderNode(node.no, (newNoNode) => {
                   updateNode({
@@ -116,9 +133,9 @@ export default function Index() {
           </div>
 
           {/* Yes branch (Right) */}
-          <div className="flex flex-col items-center w-[300px] mx-8">
-            <strong className="text-green-600 mb-2">Yes:</strong>
-            <div className="w-[300px] flex-grow">
+          <div className="flex flex-col items-center mx-8 w-[300px]">
+            <strong className="mb-2 text-green-600">Yes:</strong>
+            <div className="flex-grow w-[300px]">
               {node.yes ? (
                 renderNode(node.yes, (newYesNode) => {
                   updateNode({
@@ -151,8 +168,14 @@ export default function Index() {
     )
   }
 
+  useEffect(() => {
+    if (decisionTree) {
+      setDecisionTreeTitleDraft(decisionTree.title.value)
+    }
+  }, [decisionTree])
+
   return (
-    <div className="text-center mx-auto pt-12">
+    <div className="mx-auto pt-12 text-center">
       <header>
         <h1 className="sr-only">Decision Tree</h1>
       </header>
@@ -160,7 +183,37 @@ export default function Index() {
         <div>
           {decisionTree ? (
             <>
-              <h2 className="mb-6">{decisionTree.title.value}</h2>
+              <div className="flex items-center gap-4 mx-auto mb-6 max-w-max">
+                <div>
+                  {decisionTree.title.isEditing ? (
+                    <>
+                      <Label htmlFor="decisionTreeTitle" className="sr-only">
+                        Decision Tree Title
+                      </Label>
+                      <Input
+                        id="decisionTreeTitle"
+                        value={decisionTreeTitleDraft}
+                        placeholder="Decision Tree Title"
+                        onChange={(e) =>
+                          setDecisionTreeTitleDraft(e.target.value)
+                        }
+                      />
+                    </>
+                  ) : (
+                    <h2 className="text-2xl">{decisionTree.title.value}</h2>
+                  )}
+                </div>
+                <div>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={handleTitleEdit}
+                  >
+                    {decisionTree.title.isEditing ? <Save /> : <Pencil />}
+                  </Button>
+                </div>
+              </div>
+
               {renderNode(decisionTree.node, updateTree)}
             </>
           ) : (
