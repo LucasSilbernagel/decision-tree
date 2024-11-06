5c58e9c6ab389b57265d0199cee639cdf74fcaa8
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-11-05 23:18:39 -0500

    added FullTree tests

---
+++ app/components/FullTree/FullTree.test.tsx
@@ -0,0 +1,116 @@
+import { render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import { describe, expect, it, vi, beforeEach } from 'vitest'
+import FullTree from './FullTree'
+import { EXAMPLE_TREE } from '~/constants'
+
+const mockToast = vi.fn()
+
+vi.mock('~/hooks/use-toast', () => ({
+  useToast: () => ({
+    toast: mockToast,
+  }),
+}))
+
+vi.mock('@remix-run/react', () => ({
+  useNavigate: () => vi.fn(),
+}))
+
+describe('FullTree', () => {
+  const mockProps = {
+    decisionTree: EXAMPLE_TREE,
+    setDecisionTree: vi.fn(),
+    treeTitle: EXAMPLE_TREE.title.value,
+    setTreeTitle: vi.fn(),
+    treeHeight: 500,
+    treeWidth: 800,
+    nodePositions: new Map(),
+    setNodePositions: vi.fn(),
+    highestNodeId: 12,
+    setHighestNodeId: vi.fn(),
+    treeContainerRef: { current: document.createElement('div') },
+  }
+
+  beforeEach(() => {
+    vi.clearAllMocks()
+
+    Object.defineProperty(navigator, 'clipboard', {
+      value: {
+        writeText: vi.fn().mockImplementation(() => Promise.resolve()),
+      },
+      configurable: true,
+    })
+
+    Object.defineProperty(window, 'location', {
+      value: { href: 'http://localhost:3000' },
+      configurable: true,
+    })
+  })
+
+  it('renders share and delete buttons', () => {
+    render(<FullTree {...mockProps} />)
+
+    expect(screen.getByText('Share Tree')).toBeInTheDocument()
+    expect(screen.getByText('Start Over')).toBeInTheDocument()
+  })
+
+  it('handles share button click successfully', async () => {
+    const user = userEvent.setup()
+
+    navigator.clipboard.writeText = vi
+      .fn()
+      .mockImplementation(() => Promise.resolve())
+
+    render(<FullTree {...mockProps} />)
+
+    await user.click(screen.getByText('Share Tree'))
+
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(
+      'http://localhost:3000'
+    )
+    expect(mockToast).toHaveBeenCalledWith({
+      title: 'URL copied to clipboard!',
+    })
+  })
+
+  it('handles share button click failure', async () => {
+    const user = userEvent.setup()
+
+    navigator.clipboard.writeText = vi
+      .fn()
+      .mockImplementation(() => Promise.reject(new Error('Failed to copy')))
+
+    render(<FullTree {...mockProps} />)
+
+    await user.click(screen.getByText('Share Tree'))
+
+    expect(mockToast).toHaveBeenCalledWith({
+      title: 'Failed to copy URL',
+      variant: 'destructive',
+    })
+  })
+
+  it('handles title edit', async () => {
+    const user = userEvent.setup()
+    render(<FullTree {...mockProps} />)
+
+    await user.click(screen.getByText(mockProps.decisionTree.title.value))
+
+    expect(mockProps.setDecisionTree).toHaveBeenCalledWith({
+      ...mockProps.decisionTree,
+      title: {
+        value: mockProps.decisionTree.title.value,
+        isEditing: true,
+      },
+    })
+  })
+
+  it('updates node positions', () => {
+    render(<FullTree {...mockProps} />)
+
+    const newPosition = { x: 100, y: 100 }
+    mockProps.setNodePositions(new Map().set(1, newPosition))
+
+    expect(mockProps.setNodePositions).toHaveBeenCalled()
+  })
+})
--- app/constants/index.ts
+++ app/constants/index.ts
@@ -29,4 +29,118 @@ export const NEW_TREE = {
   },
 }
 
-export const SERIALIZED_EXAMPLE_TREE = `%7B"title"%3A"What%20should%20you%20have%20for%20lunch%3F"%2C"node"%3A%7B"id"%3A0%2C"text"%3A"Are%20you%20at%20home%3F"%2C"yes"%3A%7B"id"%3A1%2C"text"%3A"Do%20you%20have%20ingredients%20to%20make%20a%20sandwich%3F"%2C"yes"%3A%7B"id"%3A4%2C"text"%3A"Make%20a%20sandwich%20for%20lunch!"%2C"yes"%3Anull%2C"no"%3Anull%7D%2C"no"%3A%7B"id"%3A3%2C"text"%3A"Are%20you%20trying%20to%20eat%20healthy%3F"%2C"yes"%3A%7B"id"%3A6%2C"text"%3A"Order%20a%20salad%20or%20a%20rice%20bowl%20for%20delivery"%2C"yes"%3Anull%2C"no"%3Anull%7D%2C"no"%3A%7B"id"%3A5%2C"text"%3A"Order%20a%20burrito%20or%20a%20burger%20for%20delivery"%2C"yes"%3Anull%2C"no"%3Anull%7D%7D%2C"parentId"%3A0%7D%2C"no"%3A%7B"id"%3A2%2C"text"%3A"Do%20you%20want%20to%20go%20to%20a%20sit-down%20restaurant%3F"%2C"yes"%3A%7B"id"%3A8%2C"text"%3A"Are%20you%20trying%20to%20eat%20healthy%3F"%2C"yes"%3A%7B"id"%3A12%2C"text"%3A"Order%20a%20salad%2C%20or%20maybe%20some%20sushi"%2C"yes"%3Anull%2C"no"%3Anull%7D%2C"no"%3A%7B"id"%3A11%2C"text"%3A"Order%20some%20pasta"%2C"yes"%3Anull%2C"no"%3Anull%7D%7D%2C"no"%3A%7B"id"%3A7%2C"text"%3A"Are%20you%20trying%20to%20eat%20healthy%3F"%2C"yes"%3A%7B"id"%3A10%2C"text"%3A"Pick%20up%20a%20salad%20or%20a%20rice%20bowl"%2C"yes"%3Anull%2C"no"%3Anull%7D%2C"no"%3A%7B"id"%3A9%2C"text"%3A"Grab%20a%20slice%20of%20pizza!"%2C"yes"%3Anull%2C"no"%3Anull%7D%7D%2C"parentId"%3A0%7D%7D%7D`
+export const EXAMPLE_TREE = {
+  title: {
+    value: 'What should you have for lunch?',
+    isEditing: false,
+  },
+  node: {
+    id: 0,
+    text: {
+      value: 'Are you at home?',
+      isEditing: false,
+    },
+    yes: {
+      id: 1,
+      text: {
+        value: 'Do you have ingredients to make a sandwich?',
+        isEditing: false,
+      },
+      yes: {
+        id: 4,
+        text: {
+          value: 'Make a sandwich for lunch!',
+          isEditing: false,
+        },
+        yes: null,
+        no: null,
+      },
+      no: {
+        id: 3,
+        text: {
+          value: 'Are you trying to eat healthy?',
+          isEditing: false,
+        },
+        yes: {
+          id: 6,
+          text: {
+            value: 'Order a salad or a rice bowl for delivery',
+            isEditing: false,
+          },
+          yes: null,
+          no: null,
+        },
+        no: {
+          id: 5,
+          text: {
+            value: 'Order a burrito or a burger for delivery',
+            isEditing: false,
+          },
+          yes: null,
+          no: null,
+        },
+      },
+      parentId: 0,
+    },
+    no: {
+      id: 2,
+      text: {
+        value: 'Do you want to go to a sit-down restaurant?',
+        isEditing: false,
+      },
+      yes: {
+        id: 8,
+        text: {
+          value: 'Are you trying to eat healthy?',
+          isEditing: false,
+        },
+        yes: {
+          id: 12,
+          text: {
+            value: 'Order a salad, or maybe some sushi',
+            isEditing: false,
+          },
+          yes: null,
+          no: null,
+        },
+        no: {
+          id: 11,
+          text: {
+            value: 'Order some pasta',
+            isEditing: false,
+          },
+          yes: null,
+          no: null,
+        },
+      },
+      no: {
+        id: 7,
+        text: {
+          value: 'Are you trying to eat healthy?',
+          isEditing: false,
+        },
+        yes: {
+          id: 10,
+          text: {
+            value: 'Pick up a salad or a rice bowl',
+            isEditing: false,
+          },
+          yes: null,
+          no: null,
+        },
+        no: {
+          id: 9,
+          text: {
+            value: 'Grab a slice of pizza!',
+            isEditing: false,
+          },
+          yes: null,
+          no: null,
+        },
+      },
+      parentId: 0,
+    },
+  },
+}
+
+export const SERIALIZED_EXAMPLE_TREE = `%7B%22title%22%3A%22What%20should%20you%20have%20for%20lunch%3F%22%2C%22node%22%3A%7B%22id%22%3A0%2C%22text%22%3A%22Are%20you%20at%20home%3F%22%2C%22yes%22%3A%7B%22id%22%3A1%2C%22text%22%3A%22Do%20you%20have%20ingredients%20to%20make%20a%20sandwich%3F%22%2C%22yes%22%3A%7B%22id%22%3A4%2C%22text%22%3A%22Make%20a%20sandwich%20for%20lunch!%22%2C%22yes%22%3Anull%2C%22no%22%3Anull%7D%2C%22no%22%3A%7B%22id%22%3A3%2C%22text%22%3A%22Are%20you%20trying%20to%20eat%20healthy%3F%22%2C%22yes%22%3A%7B%22id%22%3A6%2C%22text%22%3A%22Order%20a%20salad%20or%20a%20rice%20bowl%20for%20delivery%22%2C%22yes%22%3Anull%2C%22no%22%3Anull%7D%2C%22no%22%3A%7B%22id%22%3A5%2C%22text%22%3A%22Order%20a%20burrito%20or%20a%20burger%20for%20delivery%22%2C%22yes%22%3Anull%2C%22no%22%3Anull%7D%7D%2C%22parentId%22%3A0%7D%2C%22no%22%3A%7B%22id%22%3A2%2C%22text%22%3A%22Do%20you%20want%20to%20go%20to%20a%20sit-down%20restaurant%3F%22%2C%22yes%22%3A%7B%22id%22%3A8%2C%22text%22%3A%22Are%20you%20trying%20to%20eat%20healthy%3F%22%2C%22yes%22%3A%7B%22id%22%3A12%2C%22text%22%3A%22Order%20a%20salad%2C%20or%20maybe%20some%20sushi%22%2C%22yes%22%3Anull%2C%22no%22%3Anull%7D%2C%22no%22%3A%7B%22id%22%3A11%2C%22text%22%3A%22Order%20some%20pasta%22%2C%22yes%22%3Anull%2C%22no%22%3Anull%7D%7D%2C%22no%22%3A%7B%22id%22%3A7%2C%22text%22%3A%22Are%20you%20trying%20to%20eat%20healthy%3F%22%2C%22yes%22%3A%7B%22id%22%3A10%2C%22text%22%3A%22Pick%20up%20a%20salad%20or%20a%20rice%20bowl%22%2C%22yes%22%3Anull%2C%22no%22%3Anull%7D%2C%22no%22%3A%7B%22id%22%3A9%2C%22text%22%3A%22Grab%20a%20slice%20of%20pizza!%22%2C%22yes%22%3Anull%2C%22no%22%3Anull%7D%7D%2C%22parentId%22%3A0%7D%7D%7D`
