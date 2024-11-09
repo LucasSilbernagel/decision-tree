f69cb38d8624efc3019c7ca149dacde3ce367a33
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-11-09 17:48:47 -0500

    started e2e testing and updating github action

---
+++ e2e/decision-tree.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(process.env.VERCEL_PREVIEW_URL || 'http://localhost:5173/')
+})
+
+test.describe('pageload', () => {
+  test('should load all landing page content as expected', async ({ page }) => {
+    await expect(page).toHaveTitle(/Home/)
+    await expect(page.getByRole('button', { name: /Example/i })).toBeVisible()
+    await expect(page.getByRole('button', { name: /New/i })).toBeVisible()
+    await expect(
+      page.getByText(
+        'A simple generator of shareable and accessible decision trees.'
+      )
+    ).toBeVisible()
+    await expect(page.getByText('Built by Lucas Silbernagel')).toBeVisible()
+  })
+})
